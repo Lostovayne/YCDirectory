d7d04458b236e3cc058f71e3687373f092f1ab02
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-27 03:39:29 -0300

    agrega shadcn y configuracion inicial

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 
 export default function Home(): React.ReactElement {
-    console.log("Welcome to Next.js")
+	console.log('Welcome to Next.js');
+	console.log('Estoy del lado del servidor');
 
-    return (
-        <>
-            <h1 className={ "text-2xl"} >
-                Home
-            </h1>
-        </>);
+	return (
+		<>
+			<section className='pink_container'>
+				<h1 className='heading'>
+					Pitch Your Startup, <br /> Connect With Entrepreneurs
+				</h1>
+				<p className='sub-heading !max-w-3xl'>
+					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+				</p>
+			</section>
+		</>
+	);
 }
