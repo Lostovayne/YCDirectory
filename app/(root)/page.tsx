cb21f7042ef5cefa80c72fd5230a162977635f46
Lostovayne <moises.barrera.dev@gmail.com>
2024-10-27 01:25:19 -0300

    first commit

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function Home(): React.ReactElement {
+    console.log("Welcome to Next.js")
+
+    return (
+        <>
+            <h1 className={ "text-2xl"} >
+                Home
+            </h1>
+        </>);
+}
