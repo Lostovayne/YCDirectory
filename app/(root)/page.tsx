f06e1e85bd2052527965f66df186dd8f94a8e2a7
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-16 19:58:21 -0300

    agrega search query

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -4,13 +4,17 @@ import { STARTUPS_QUERY } from '@/lib/queries';
 import { sanityFetch, SanityLive } from '@/sanity/lib/live';
 import React from 'react';
 
-export default async function Home({
-	searchParams,
-}: {
-	searchParams: Promise<{ query?: string }>;
-}): Promise<React.ReactNode> {
+export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }): Promise<React.ReactNode> {
+	/**
+	 *
+	 * Extrae el parámetro de búsqueda de searchParams y crea un objeto de parámetros
+	 * @param {Promise<{ query?: string }>} searchParams - Los parámetros de búsqueda de la URL
+	 * @returns {Object} params - Objeto con el parámetro de búsqueda o null
+	 *
+	 */
 	const query = (await searchParams).query;
-	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY });
+	const params = { search: query || null };
+	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
 	return (
 		<>
@@ -18,9 +22,7 @@ export default async function Home({
 				<h1 className='heading'>
 					Pitch Your Startup, <br /> Connect With Entrepreneurs
 				</h1>
-				<p className='sub-heading !max-w-3xl'>
-					Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
-				</p>
+				<p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
 				<SearchForm query={query} />
 			</section>
 
@@ -28,12 +30,7 @@ export default async function Home({
 				<p className='text-30-semibold'>{query ? `Search Results for "${query}"` : `All Startups`}</p>
 				<ul className='mt-7 card_grid'>
 					{posts?.length > 0 ?
-						posts.map((post: StartupTypeCard) => (
-							<StartupCard
-								key={post?._id}
-								post={post}
-							/>
-						))
+						posts.map((post: StartupTypeCard) => <StartupCard key={post?._id} post={post} />)
 					:	<p className='no-results'>No Startups Found </p>}
 				</ul>
 			</section>
