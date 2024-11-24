5e8bac6e2609d2210a40d71054a0624cf6773b96
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-24 04:12:41 -0300

    agregada la vista de los post individuales

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -2,6 +2,8 @@
 import { STARTUP_BY_ID_QUERY } from "@/lib/queries";
 import { formDate } from "@/lib/utils";
 import { client } from "@/sanity/lib/client";
+import markdownit from "markdown-it";
+import Image from "next/image";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 import { FC, ReactElement } from "react";
@@ -12,9 +14,10 @@ interface StartupPageProps {
   params: Promise<{ id: string }>;
 }
 
+const md = markdownit();
+
 const StartupPage: FC<StartupPageProps> = async ({ params }): Promise<ReactElement> => {
   const id = (await params).id;
-  console.log(id);
 
   /**
    *
@@ -27,6 +30,8 @@ const StartupPage: FC<StartupPageProps> = async ({ params }): Promise<ReactEleme
   const post = await client.fetch(STARTUP_BY_ID_QUERY, { id });
   if (!post) return notFound();
 
+  const parsedContent = md.render(post?.pitch || "");
+
   return (
     <>
       <section className="pink_container !min-h-[230px]">
@@ -36,12 +41,40 @@ const StartupPage: FC<StartupPageProps> = async ({ params }): Promise<ReactEleme
       </section>
 
       <section className="section_container">
-        <img src={post.image} alt={`thmbnail of ${post.title}`} className="w-full h-auto rounded-xl" />
+        <img
+          src={post.image}
+          alt={`thmbnail of ${post.title}`}
+          className="w-full aspect-video object-cover h-auto rounded-xl"
+        />
         <div className="space-y-5 mt-10 max-w-4xl mx-auto">
           <div className="flex-between gap-5">
-            <Link href={`/user/${post.author?._id}`}></Link>
+            <Link href={`/user/${post.author?._id}`} className="flex gap-2 items-center mb-3">
+              <Image
+                src={post.author?.image}
+                alt={post.author?.name}
+                width={64}
+                height={64}
+                className="rounded-full drop-shadow-lg"
+              />
+              <div>
+                <p className="text-20-medium">{post.author?.name}</p>
+                <p className="text-16-medium !text-black-300">@{post.author?.username}</p>
+              </div>
+            </Link>
+            <p className="category-tag">{post.category}</p>
           </div>
+          <h3 className="text-30-bold">Pitch Details</h3>
+
+          {parsedContent ?
+            <article
+              className="prose max-w-4xl font-work-sans break-all"
+              dangerouslySetInnerHTML={{ __html: parsedContent }}
+            />
+          : <p className="no-results">No details provided</p>}
         </div>
+        <hr className="divider" />
+
+        {/* Editor selected startup */}
       </section>
     </>
   );
