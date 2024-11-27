f12b1bdf1addc660b1729cf6100d0196f5da8a20
Lostovayne <moises.barrera.dev@gmail.com>
2024-11-27 00:48:47 -0300

    feat(auth): implement authentication and session management

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,41 +1,52 @@
-import SearchForm from '@/components/SearchForm';
-import StartupCard, { type StartupTypeCard } from '@/components/StartupCard';
-import { STARTUPS_QUERY } from '@/lib/queries';
-import { sanityFetch, SanityLive } from '@/sanity/lib/live';
-import React from 'react';
+import { auth } from "@/auth";
+import SearchForm from "@/components/SearchForm";
+import StartupCard, { type StartupTypeCard } from "@/components/StartupCard";
+import { STARTUPS_QUERY } from "@/lib/queries";
+import { sanityFetch, SanityLive } from "@/sanity/lib/live";
+import React from "react";
 
-export default async function Home({ searchParams }: { searchParams: Promise<{ query?: string }> }): Promise<React.ReactNode> {
-	/**
-	 *
-	 * Extrae el parámetro de búsqueda de searchParams y crea un objeto de parámetros
-	 * @param {Promise<{ query?: string }>} searchParams - Los parámetros de búsqueda de la URL
-	 * @returns {Object} params - Objeto con el parámetro de búsqueda o null
-	 *
-	 */
-	const query = (await searchParams).query;
-	const params = { search: query || null };
-	const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
+export default async function Home({
+  searchParams,
+}: {
+  searchParams: Promise<{ query?: string }>;
+}): Promise<React.ReactNode> {
+  /**
+   *
+   * Extrae el parámetro de búsqueda de searchParams y crea un objeto de parámetros
+   * @param {Promise<{ query?: string }>} searchParams - Los parámetros de búsqueda de la URL
+   * @returns {Object} params - Objeto con el parámetro de búsqueda o null
+   *
+   */
+  const query = (await searchParams).query;
+  const params = { search: query || null };
 
-	return (
-		<>
-			<section className='pink_container'>
-				<h1 className='heading'>
-					Pitch Your Startup, <br /> Connect With Entrepreneurs
-				</h1>
-				<p className='sub-heading !max-w-3xl'>Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.</p>
-				<SearchForm query={query} />
-			</section>
+  const session = await auth();
+  console.log(session?.id);
 
-			<section className='section_container'>
-				<p className='text-30-semibold'>{query ? `Search Results for "${query}"` : `All Startups`}</p>
-				<ul className='mt-7 card_grid'>
-					{posts?.length > 0 ?
-						posts.map((post: StartupTypeCard) => <StartupCard key={post?._id} post={post} />)
-					:	<p className='no-results'>No Startups Found </p>}
-				</ul>
-			</section>
+  const { data: posts } = await sanityFetch({ query: STARTUPS_QUERY, params });
 
-			<SanityLive />
-		</>
-	);
+  return (
+    <>
+      <section className="pink_container">
+        <h1 className="heading">
+          Pitch Your Startup, <br /> Connect With Entrepreneurs
+        </h1>
+        <p className="sub-heading !max-w-3xl">
+          Submit Ideas, Vote on Pitches, and Get Noticed in Virtual Competitions.
+        </p>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">{query ? `Search Results for "${query}"` : `All Startups`}</p>
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ?
+            posts.map((post: StartupTypeCard) => <StartupCard key={post?._id} post={post} />)
+          : <p className="no-results">No Startups Found </p>}
+        </ul>
+      </section>
+
+      <SanityLive />
+    </>
+  );
 }
--- sanity/env.ts
+++ sanity/env.ts
@@ -10,10 +10,7 @@ export const projectId = assertValue(
   "Missing environment variable: NEXT_PUBLIC_SANITY_PROJECT_ID"
 );
 
-export const token = assertValue(
-  process.env.SANITY_WRITE_TOKEN,
-  "Missing environment variable: NEXT_PUBLIC_SANITY_TOKEN"
-);
+export const token = process.env.SANITY_WRITE_TOKEN;
 
 function assertValue<T>(v: T | undefined, errorMessage: string): T {
   if (v === undefined) {
