76231e0a98b98160cb9048498e7981fb526f07fb
Lostovayne <moises.barrera.dev@gmail.com>
2024-12-06 16:07:51 -0300

    agrega user startups

--- app/(root)/user/[id]/page.tsx
+++ app/(root)/user/[id]/page.tsx
@@ -5,6 +5,8 @@ import Image from "next/image";
 import { notFound } from "next/navigation";
 import { ReactElement } from "react";
 
+export const experimental_ppr = true;
+
 const UserPage = async ({ params }: { params: Promise<{ id: string }> }): Promise<ReactElement> => {
   const id = (await params).id;
   const session = await auth();
@@ -22,6 +24,11 @@ const UserPage = async ({ params }: { params: Promise<{ id: string }> }): Promis
           </div>
           <Image src={user.image} alt={user.name} width={220} height={220} className="profile_image" />
           <p className="text-30-extrabold mt-7 text-center">@{user.username}</p>
+          <p className="mt-1 text-center text-14-normal">{user.bio}</p>
+        </div>
+        <div className="flex-1 flex flex-col gap-5 lg:mt-5">
+          <p className="text-30-bold">{session?.id === id ? "Your" : "All"} Startups</p>
+          <ul className="card_grid-sm">{/* TODO:ADD USER_STARTUPS */}</ul>
         </div>
       </section>
     </>
